aaf6654f8d8ea98a6138d99c60744ec27a5d62f3
Trygve Wastvedt <twastvedt@azahner.com>
2017-08-12 17:17:09 -0500

    Add properties to three-vrcontrols.

--- types/three/three-vrcontrols.d.ts
+++ types/three/three-vrcontrols.d.ts
@@ -12,7 +12,26 @@ export class VRControls {
 
     zeroSensor(): void;
 
+    /**
+     * The Rift SDK returns the position in meters. This scale factor allows the user to define how meters are converted to scene units.
+     */
     scale: number;
+
+    /**
+     * If true will use "standing space" coordinate system where y=0 is the floor and x=0, z=0 is the center of the room.
+     */
+    standing: boolean;
+
+	/**
+     * Distance from the users eyes to the floor in meters. Used when standing=true but the VRDisplay doesn't provide stageParameters.
+     */
+    userHeight: number;
+
+    getVRDisplay(): VRDisplay;
+
+    getStandingMatrix(): THREE.Matrix4;
+
+    dispose(): void;
 
     setVRDisplay(display: VRDisplay): void;
 }
